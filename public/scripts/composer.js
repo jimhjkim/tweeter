cc262a302ab442996e2a81672f7fd4d38c168ed1
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-10-10 15:39:06 -0400

    fixed user input text area disappearing when user clicks the scroll up button

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,31 +1,36 @@
 // listen for scrolling
 // let position = $(window).scrollTop();
+let shouldHide = true;
 
 $(window).scroll(function(event) {
   let scroll = $(window).scrollTop();
 
   // near the bottom
-  if (scroll > 400) {
-    $('.new-tweet').slideUp('slow', () => {
-      $('#scroll-up').fadeIn('slow');
-      $('nav div').fadeOut('slow');
+  if (scroll > 300) {
+    if (shouldHide) {
+      $('.new-tweet').slideUp('slow');
+    }
+    $('#scroll-up').fadeIn('slow');
+    $('nav div').fadeOut('slow');
+
       // listen for click
       $('#scroll-up').click(() => {
+        shouldHide = false;
         window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
         $('.new-tweet').fadeIn(2000, () => {
           $('textarea').focus();
+          shouldHide = true;
         });
       });
-    });
-
   // near the top
   } else {
     $('#scroll-up').fadeOut('slow');
     $('nav div').fadeIn('slow');
 
   }
-  // position = scroll;
 });
+  // position = scroll;
+
 
 
   
